4cef1d8a771897672da456373b27c924bd90d4f3
RyanNepton1 <Ryan.nepton@mail.mcgill.ca>
2026-01-22 15:52:17 -0500

    update help

--- Assignment_1_Winter2026/project/src/interpreter.c
+++ Assignment_1_Winter2026/project/src/interpreter.c
@@ -88,7 +88,8 @@ quit			Exits / terminates the shell with “Bye!”\n \
 set VAR STRING		Assigns a value to shell memory\n \
 print VAR		Displays the STRING assigned to VAR\n \
 source SCRIPT.TXT	Executes the file SCRIPT.TXT\n \
-echo STRING/VAR	Prints string or string corresponding to variable\n ";
+echo STRING/VAR	Prints string or string corresponding to variable\n \
+my_ls             Lists all files and directories in the current directory\n ";
     printf("%s\n", help_string);
     return 0;
 }
