3964920bfe0dabdb8c7262918f480828ab27289c
RyanNepton1 <Ryan.nepton@mail.mcgill.ca>
2026-01-22 15:54:29 -0500

    update help

--- Assignment_1_Winter2026/project/src/interpreter.c
+++ Assignment_1_Winter2026/project/src/interpreter.c
@@ -89,7 +89,7 @@ set VAR STRING		Assigns a value to shell memory\n \
 print VAR		Displays the STRING assigned to VAR\n \
 source SCRIPT.TXT	Executes the file SCRIPT.TXT\n \
 echo STRING/VAR	Prints string or string corresponding to variable\n \
-my_ls               Lists all files and directories in the current directory\n ";
+my_ls			Lists all files and directories in the current directory\n ";
     printf("%s\n", help_string);
     return 0;
 }
